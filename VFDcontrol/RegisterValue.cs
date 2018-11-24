091199a957c5f9a18110cc1e4ea0fa076733cf09
Thomas Pedersen <thomas@copenhagenatomics.com>
2018-11-24 20:30:36 +0100

    More accurate upload package length.

--- VFDcontrol/RegisterValue.cs
+++ VFDcontrol/RegisterValue.cs
@@ -302,6 +302,7 @@ namespace VFDcontrol
             var lines = LoadCsv(fileName, csvSeperator);
             if (lines != null)
             {
+                Debug.Print("================== Startin upload ======================");
                 foreach (var line in lines.Where(x => x.DefaultValue != "Unknown"))
                 {
                     try
@@ -690,108 +691,7 @@ namespace VFDcontrol
         {
             get
             {
-                switch (ID)
-                {
-                    case 0: 
-                    case 1: 
-                    case 2: return 2;
-                    case 3:
-                    case 4:
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                    case 11: return 3;
-                    case 12:
-                    case 13:
-                    case 22:
-                    case 23:
-                    case 24:
-                    case 25:
-                    case 26: return 2;
-                    case 27:
-                    case 28: return 3;
-                    case 29:
-                    case 30:
-                    case 31:
-                    case 32:
-                    case 33:
-                    case 34:
-                    case 35:
-                    case 41: return 2;
-                    case 42: return 2;
-                    case 43: return 2;
-                    case 44:
-                    case 45:
-                    case 46:
-                    case 47:
-                    case 48:
-                    case 49:
-                    case 50:
-                    case 51:
-                    case 52:
-                    case 53:
-                    case 54: return 2;
-                    case 55:
-                    case 56:
-                    case 57:
-                    case 58:
-                    case 59:
-                    case 60:
-                    case 61:
-                    case 62: return 3;
-                    case 63: return 2;
-                    case 64: return 2;
-                    case 65: return 2;
-                    case 66: return 2;
-                    case 67:
-                    case 68:
-                    case 69:
-                    case 70:
-                    case 71: return 2;
-                    case 72:
-                    case 73: return 3;
-                    case 74:
-                    case 75:
-                    case 76:
-                    case 77:
-                    case 78:
-                    case 79:
-                    case 80:
-                    case 81:
-                    case 82:
-                    case 83: return 2;
-                    case 84:
-                    case 85: return 2;
-                    case 86:
-                    case 87:
-                    case 88:
-                    case 89:
-                    case 90:
-                    case 91:
-                    case 92:
-                    case 93:
-                    case 94:
-                    case 95:
-                    case 96:
-                    case 97:
-                    case 98:
-                    case 99:
-                    case 100: return 3;
-                    case 101:
-                    case 102:
-                    case 103:
-                    case 104:
-                    case 105:
-                    case 106:
-                    case 107:
-                    case 108:return 2;
-                    default: return 2;
-                }
-
-                throw new NotImplementedException();
+                return Type == "byte" ? 2 : 3;
             }
         }
 
