ae48948177bbae259b969312f391be64a7bd487a
Thomas Pedersen <thomas@copenhagenatomics.com>
2019-10-16 21:13:49 +0200

    minor cleanup...

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,10 +1,7 @@
 ﻿using CA_DataUploaderLib;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using VFDcontrol;
 
 namespace ConsoleTest
